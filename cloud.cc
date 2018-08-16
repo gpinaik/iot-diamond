4646aa317d885ec7b84ae2c3c329ce99bec01d3e
Guruprasad I Naik <guru.prasad.naik@huawei.com>
2018-08-16 10:19:04 +0530

    sample edge and cloud based on diamond

---
+++ cloud.cc
@@ -0,0 +1,111 @@
+#include <boost/program_options.hpp>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unistd.h>
+
+#include <includes/data_types.h>
+#include <includes/transactions.h>
+
+namespace po = boost::program_options;
+using namespace std;
+using namespace diamond;
+
+DStringList cloudList;
+DStringList edgeList;
+DStringList deviceList;
+DList deviceStatusList;
+DCounter uniqueId; 
+
+int main(int argc, char ** argv) {
+    string configPrefix;
+    string myName, key;
+
+    // gather commandline options
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help",
+         "produce help message")
+        ("key",
+         po::value<std::string>(&key)->required(),
+         "iot user name")
+        ("name",
+         po::value<std::string>(&myName)->required(),
+         "cloud name")
+        ("config",
+         po::value<std::string>(&configPrefix)->required(),
+         "frontend config file prefix (required)");
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    if (vm.count("help")) {
+        cout << desc << endl;
+        exit(1);
+    }
+    po::notify(vm);
+
+    //set up diamond
+    DiamondInit(configPrefix, 1, 0);
+    StartTxnManager();
+
+    // Map iot state
+    DObject::Map(cloudList, key + "iot:cloudList");
+    DObject::Map(edgeList, key + "iot:edgeList");
+    DObject::Map(deviceList, key + "iot:deviceList");
+    DObject::Map(deviceStatusList, key + "iot:deviceStatusList");
+    DObject::Map(uniqueId, key + "iot:uniqueId");
+
+    // Add cloud
+    execute_txn([myName] () {
+        if (cloudList.Index(myName) == -1) cloudList.Append(myName);
+    },
+    // txn callback: If transaction fails, exit
+    [myName] (bool committed) { 
+        if (!committed) exit(1);
+    });
+    // Set up our reactive display
+    uint64_t reactive_id =
+    reactive_txn([myName] () {
+        // print out current device id and cloud and edge
+        cout << "Unique Id: " << uniqueId.Value() << endl;
+        cout << "Cloud Nodes: ";
+        for (auto &p : cloudList.Members())
+              cout << p << " ";
+        cout << endl;
+        cout << "Edge Nodes: ";
+        for (auto &p : edgeList.Members())
+              cout << p << " ";
+        cout << endl;
+        cout << "Devices: " << endl;
+        for (auto &p : deviceList.Members()){
+              	cout << "Name:" << p << " ";
+		int index = deviceList.Index(p);
+		if (index != -1)
+		if (deviceStatusList.Value(index) != -1)
+              		cout << "Status:" <<  deviceStatusList.Value(index);
+		cout << endl;
+	}
+        cout << endl;
+        cout << "Enter device name: " << flush;
+    });
+
+    // Cycle on user input to add device
+    while (1) {
+	string deviceName;
+	cin >> deviceName;
+        // Add device
+        execute_txn([myName,deviceName] () {
+            if (deviceList.Index(deviceName) == -1){
+		deviceList.Insert(uniqueId.Value(),deviceName);
+		deviceStatusList.Insert(uniqueId.Value(),2);
+	    }
+	    ++uniqueId;
+        },
+        // txn callback: If transaction fails, exit
+        [] (bool committed) { 
+            if (!committed) exit(1);
+        });
+    }
+    return 0;
+}
